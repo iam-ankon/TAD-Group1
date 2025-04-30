ea9473a454adb3c1551d4523bbf1d03573bf29c5
iam-ankon <ar7web47@gamil.com>
2025-04-30 16:53:06 +0600

    commit

--- src/components/hr/EmployeeDetails.jsx
+++ src/components/hr/EmployeeDetails.jsx
@@ -164,6 +164,7 @@ const EmployeeDetails = () => {
                     <th>Designation</th>
                     <th>Department</th>
                     <th>Company</th>
+                    <th>Joning Date</th>
                     <th>Actions</th>
                   </tr>
                 </thead>
@@ -180,6 +181,7 @@ const EmployeeDetails = () => {
                         <td>{employee.designation}</td>
                         <td>{employee.department}</td>
                         <td>{employee.company_name}</td>
+                        <td>{new Date(employee.joining_date).toLocaleDateString()}</td>
                         <td className="action-buttons-cell">
                           <button
                             onClick={(e) => {
